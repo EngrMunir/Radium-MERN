c82866c0f36db113a929f3ea98042ac59f936ec0
munir <smsirajulmonir@gmail.com>
2025-02-17 08:20:00 +0600

    image upload success

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,7 +1,7 @@
-import { FieldValues, SubmitHandler } from "react-hook-form";
+import { Controller, FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button, Col, Divider, Row } from "antd";
+import { Button, Col, Divider, Form, Input, Row } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 import { bloodGroupOptions, genderOptions } from "../../../components/constants/global";
 import PHDatePicker from "../../../components/form/PHDatePicker";
@@ -33,6 +33,7 @@ const CreateStudent = () => {
         const formData = new  FormData();
 
         formData.append('data', JSON.stringify(studentData));
+        formData.append('file', data.image);
 
         addStudent(formData);
 
@@ -64,6 +65,19 @@ const CreateStudent = () => {
                     </Col>
                     <Col span={24} md={{span:12}} lg={{span:8}}>
                         <PHSelect options={bloodGroupOptions} name="bloodGroup" label="Blood Group"/>
+                    </Col>
+                    <Col span={24} md={{span:12}} lg={{span:8}}>
+                    <Controller name="image" render={({field: {onChange, value, ...field}})=>(
+                        <Form.Item label="Picture">
+                            <Input 
+                            type="file"
+                            value={value?.fileName} 
+                            {...field} 
+                            onChange={(e)=> onChange(e.target.files?.[0])}
+                            />
+                        </Form.Item>
+                    ) }/>
+                        
                     </Col>
                </Row>
                 <Divider>Contact Info</Divider>
